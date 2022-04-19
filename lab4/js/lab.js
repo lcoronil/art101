48dfb4a36c92ff79299308220ebed792562d7126
Lee Coronilia <shadowwolf246815@gmail.com>
2022-04-19 15:06:47 -0700

    Lab 4 and 5 Finished

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+// Print hang-man base template before each turn
+  // Generate 1 phrase of 5
+  // Accept user input (letters)
+  // Validate user move
+  // Evaluate possible values (check if letter is part of word)
+  // Accept correct values (fill in part of word with user’s input)
+  // Print single shape if value input is correct
+  // Test win or loss conditions
---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/**
+ * Author:    Lee Coronilia
+ * Created:   19 April
+ * License: Public Domain
+ **/
+
+// Define Variables
+make = "Honda";
+model = "Civic";
+color = "White";
+year = 2020;
+
+ownIt = true;
+
+// calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
