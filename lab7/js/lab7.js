e7d56c9b7ce76c43f277645323e354a75488daf5
Lee Coronilia <shadowwolf246815@gmail.com>
2022-04-27 20:31:48 -0700

    added css, did function

---
+++ lab7/js/lab7.js
@@ -0,0 +1,24 @@
+/**
+ * Author:    Lee Coronilia
+ * Created:   4.27.2022
+ * Licensse: Public Domain
+ **/
+
+function sortUser() {
+  var userName = prompt("Hey, please give me your name so I can fix it.");
+  console.log("userName =", userName);
+  //split string to Array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  //sort the Array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+//output
+document.writeln("Here's your fixed name: ",
+    sortUserName(), "</br>");
