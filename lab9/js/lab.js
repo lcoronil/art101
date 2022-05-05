ddbda9507df04d0b55b6a923fff3f4ca045bf962
Lee Coronilia <shadowwolf246815@gmail.com>
2022-05-04 18:08:27 -0700

    finished lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,18 +4,20 @@
  * Licensse: Public Domain
  **/
 
-// output div
-outputEl = document.getElementById("output");
- // new div
-new1El = document.createElement("p");
-
+  // assign output div to outputEl
+var outputEl = document.getElementById("output");
+  // create new element named new1El
+var new1El = document.createElement("p");
+  // change html attribute to say something new ?
 new1El.innerHTML = "hello";
-
-new2El = document.createElement("h");
-
+  // create another new element named new2El
+var new2El = document.createElement("h");
+  // change html attrib of new2El to say something else ?
 new2El.innerHTML = "What's up";
+  // append both new elements to output <div>
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
 
-outputEl.appendChild(new1El, new2El);
-
+  // change font size
 new1El.style.fontSize = "20pt";
 new2El.style.dontSize = "20pt";
